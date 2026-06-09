465ff01122f6eeb1051ad780a6db1796bfdb7d57
khanubed <ubedprogrammer@gmail.com>
2026-06-09 17:56:53 +0530

    vercol.json upload client

--- Server/src/routes/order.routes.js
+++ Server/src/routes/order.routes.js
@@ -3,14 +3,12 @@ import {
   cancelOrder,
   createOrder,
   getUserOrders,
-  placeOrder,
   verifyPaymentSignature,
 } from "../controllers/order.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/place", protect, placeOrder);
 orderRouter.get("/my-orders", protect, getUserOrders);
 orderRouter.patch("/:orderId/cancel", protect, cancelOrder);
 orderRouter.post("/checkout", protect, createOrder);
